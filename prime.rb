e7e4c2a5e233b503cbb2aa326ad6980a4ddcc64c
Brian Firestone <bfirestone2339@gmail.com>
2020-12-10 18:52:50 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+    if num <= 1
+        return false
+    else
+        (2..(num - 1)).each do |x|
+            return false if num % x == 0
+        end
+    return true
+    end
+end
+
