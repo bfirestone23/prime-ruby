47b004ff0dd45b5ef935811f245ab58e733dc27c
Brian Firestone <bfirestone2339@gmail.com>
2020-12-10 18:56:37 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -4,10 +4,9 @@ def prime?(num)
     if num <= 1
         return false
     else
-        (2..(num - 1)).each do |x|
-            return false if num % x == 0
+        (2..(num - 1)).to_a.all? do |x|
+            num % x != 0
         end
-    return true
     end
 end
 
